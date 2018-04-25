059728b74d86412ac58fd0bc1a6ee55a96b70f87
Tomohiro Mitani <tmitani@tmitani-ltm2.internal.salesforce.com>
2018-04-25 13:59:58 +0900

    message change

--- src/test/java/hello/HttpRequestTest.java
+++ src/test/java/hello/HttpRequestTest.java
@@ -25,6 +25,6 @@ public class HttpRequestTest {
     @Test
     public void greetingShouldReturnDefaultMessage() throws Exception {
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
-                String.class)).contains("Hello World!");
+                String.class)).contains("Hello World Tokyo DevOps!");
     }
 }
